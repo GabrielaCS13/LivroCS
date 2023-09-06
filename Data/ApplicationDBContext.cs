22d50845b89e0b857826bf5e7ef2f8d73b357790
GabrielaCS13 <gabriela.silva.00243@gmail.com>
2023-09-06 13:54:47 -0300

    Adicionando o arquivo

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Livro.Models;
+
+namespace Livro.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+            public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+            {
+
+            }
+            public DbSet<livro> Livro { get; set; }
+        
+    }
+}
